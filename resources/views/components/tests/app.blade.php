746c044980105bd3539b9cd061dc085f834bd103
sezan-taso <kurosakisezan3@gmail.com>
2022-12-24 15:06:56 +0900

    名前付きスロットの追記

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -14,6 +14,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
+        {{ $header }}
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,5 @@
 <x-tests.app>
+	<x-slot name="header">ヘッダー１</x-slot>
+
 	コンポーネント１
 </x-tests>
